73bea6b89ffc0ef38a0ed4b25bba343d279ca3a1
Eyal.Kapah <eykapah@microsoft.com>
2021-08-13 09:44:07 +0300

    Tweaking the User table and Stored Procedure

--- StockWatcher.Database/dbo/Stored Procedures/spUsersInsert.sql
+++ StockWatcher.Database/dbo/Stored Procedures/spUsersInsert.sql
@@ -1,10 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[spUsersInsert]
-	--@param1 int = 0,
-	--@param2 int
+	@Email NVARCHAR(50),
+	@Password NVARCHAR(128),
+	@FirstName NVARCHAR(50),
+	@LastName NVARCHAR(50)
 AS
 BEGIN
 	--SELECT @param1, @param2
 	INSERT INTO dbo.Users([Email], [Password], [FirstName], [LastName])
-	VALUES('kaycee1@gmail.com', '1122', 'Eyal', 'Kapah');
+	VALUES(@Email, @Password, @FirstName, @LastName);
 
 END
